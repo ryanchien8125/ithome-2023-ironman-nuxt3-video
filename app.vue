718b2a2fc37ed76939f0409a03919e7fd6cf1a77
Ryan <ryanchien8125@gmail.com>
2023-09-19 00:06:28 +0800

    使用 Tailwind CSS

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="bg-white py-24">
+    <div class="flex flex-col items-center">
+      <h1 class="text-6xl font-semibold text-blue-600">2023 iThome</h1>
+      <p class="mt-4 text-9xl font-bold">鐵人賽</p>
+    </div>
   </div>
 </template>
