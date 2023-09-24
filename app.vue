01de4dd471687cc95c65e7d3c9a625aefd83dc55
Ryan <ryanchien8125@gmail.com>
2023-09-25 02:49:02 +0800

    建立自動載入的元件

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <IronManWelcome />
+    <CustomHelloWorld />
   </div>
 </template>
---
+++ components/Custom/HelloWorld.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="bg-white py-24">
+    <div class="flex flex-col items-center">
+      <h1 class="text-6xl font-semibold text-sky-400">Hello World!</h1>
+    </div>
+  </div>
+</template>
