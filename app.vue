c05981b35ae2f8710638057efc53073f50fe2120
Ryan <ryanchien8125@gmail.com>
2023-09-26 19:38:50 +0800

    使用 Nuxt Icon 模組建立圖示

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="flex flex-col items-center">
+    <Icon name="logos:nuxt" size="240" />
+    <Icon class="h-24 w-24 text-yellow-400" name="heroicons:sun" />
+    <Icon name="NuxtIcon" size="128" />
   </div>
 </template>
