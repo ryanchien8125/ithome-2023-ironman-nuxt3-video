954124b9d2861a61a9f7d46b0c61536cf523f2d9
Ryan <ryanchien8125@gmail.com>
2023-10-01 16:02:23 +0800

    使用 useState

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-white py-24">
+    <div class="flex flex-col items-center">
+      <h1 class="text-6xl font-semibold text-gray-800">這裡是首頁</h1>
+      <div class="my-4 flex flex-col space-y-4">
+        <NuxtLink to="/count/ref">前往 /count/ref</NuxtLink>
+        <NuxtLink to="/count/useState">前往 /count/useState</NuxtLink>
+        <NuxtLink to="/counter/increment">前往 /counter/increment</NuxtLink>
+        <NuxtLink to="/counter/surprise">前往 /counter/surprise</NuxtLink>
+        <NuxtLink to="/locale">前往 /locale</NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ pages/locale.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-white py-24">
+    <div class="flex flex-col items-center">
+      <p class="mt-4 text-xl text-slate-500">Locale</p>
+      <span class="text-6xl font-semibold text-emerald-400">{{ locale }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const locale = useLocale()
+</script>
