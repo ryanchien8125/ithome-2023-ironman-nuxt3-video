a02705cc098204780a68f90cb7978a99a46e0756
Ryan <ryanchien8125@gmail.com>
2023-10-06 19:36:38 +0800

    使用 Nuxt i18n 套件實現多國語系

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
-  devtools: { enabled: true }
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n'],
+  devtools: { enabled: true },
+  i18n: {
+    langDir: 'locales',
+    locales: [
+      {
+        code: 'en',
+        iso: 'en-US',
+        file: 'en.json'
+      },
+      {
+        code: 'zh',
+        iso: 'zh-TW',
+        file: 'zh.json'
+      }
+    ],
+    defaultLocale: 'zh',
+    strategy: 'no_prefix',
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+      redirectOn: 'root'
+    }
+  }
 })
