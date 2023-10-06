e4f08d2aeb3158fe3ece26401d12ea0efbc1849d
Ryan <ryanchien8125@gmail.com>
2023-10-07 06:42:42 +0800

    靜態資源管理

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="flex flex-row flex-wrap gap-4">
+    <img src="/bg.jpg" width="600" />
+    <img src="~/assets/cat.jpg" width="600" />
+    <img src="@/assets/cat.jpg" width="600" />
+    <img :src="catImage" width="600" />
   </div>
 </template>
+
+<script setup>
+import catImage from 'assets/cat.jpg'
+</script>
